2ddbc28af02b0d4ebc8a67fcaefa1eeb76e5197e
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-01-24 23:26:44 +0000

    Fix #7

--- src/features/expansion/ExpansionSelector.tsx
+++ src/features/expansion/ExpansionSelector.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { useAppDispatch, useAppSelector } from "src/app/hooks";
-import { EXPANSION } from "src/constants/game/expansion.consts";
+import { EXPANSION, Expansion } from "src/constants/game/expansion.consts";
 import { selectExpansion, setExpansion } from "./expansionSelectorSlice";
+import { selectPlayerClass, setPlayerClass } from "../class/playerClassSelectorSlice";
+import { classesAllowedInExpansions } from "src/constants/game/expansion-classes.consts";
 
 // type ExpansionSelectorProps = {};
 
 // export const ExpansionSelector = (props: ExpansionSelectorProps) => {
 export const ExpansionSelector = () => {
-    const expansion = useAppSelector(selectExpansion);
+    const selectedPlayerClass = useAppSelector(selectPlayerClass);
+    const selectedExpansion = useAppSelector(selectExpansion);
     const dispatch = useAppDispatch();
+
+    const handleSetExpansionOnClick = (expansionSelection: Expansion) => {
+        // should be fine, there shouldn't be duplicate expansion names
+        const selectedExpansionAllowedClasses = classesAllowedInExpansions.filter(
+            (item) => item.expansion === expansionSelection
+        )[0].allowedClasses;
+
+        // we make sure that the class is actually allowed in the expansion we pick
+        if (!selectedExpansionAllowedClasses.includes(selectedPlayerClass)) {
+            dispatch(setPlayerClass(selectedExpansionAllowedClasses[0]));
+        }
+
+        dispatch(setExpansion(expansionSelection));
+    };
 
     return (
         <>
-            <button onClick={() => dispatch(setExpansion(EXPANSION.CLASSIC))}>Classic</button>
-            <button onClick={() => dispatch(setExpansion(EXPANSION.WOTLK))}>Wrath</button>
-            <button onClick={() => dispatch(setExpansion(EXPANSION.DRAGONFLIGHT))}>
+            <button onClick={() => handleSetExpansionOnClick(EXPANSION.CLASSIC)}>Classic</button>
+            <button onClick={() => handleSetExpansionOnClick(EXPANSION.WOTLK)}>Wrath</button>
+            <button onClick={() => handleSetExpansionOnClick(EXPANSION.DRAGONFLIGHT)}>
                 Dragonflight
             </button>
             <br />
-            <p>Selelected expansion is: {expansion}</p>
+            <p>Selelected expansion is: {selectedExpansion}</p>
         </>
     );
 };
