fc578ae885332c6da90730b75ee28fb463a2f6a1
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-02-07 22:50:01 +0000

    Make layout full height

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ type Props = { children?: React.ReactNode };
 
 export const MainLayout = (props: Props) => {
     return (
-        <main className="dark:bg-blue-950 dark:text-white">
+        <main className="dark:bg-blue-950 dark:text-white h-screen">
             <Header />
             <Outlet />
         </main>
