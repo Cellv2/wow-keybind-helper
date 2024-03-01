6aeab6fc09439f9150cd4363c13eef61ff90e777
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-03-01 21:05:19 +0000

    Fix #20

--- src/features/profession/ProfessionSelectionButton.tsx
+++ src/features/profession/ProfessionSelectionButton.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
-type Props = { professionName: string };
+type Props = {
+    professionName: string;
+    selections: string[];
+    handleOnClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
 
-export const ProfessionSelectionButton = (props: Props) => {
-    return <div>{props.professionName}</div>;
+export const ProfessionSelectionButton = ({ professionName, selections, handleOnClick }: Props) => {
+    return (
+        <>
+            <button onClick={(e) => handleOnClick(e)} value={professionName}>
+                {professionName} - {selections.includes(professionName) ? "✅" : "❌"}
+            </button>
+            <br />
+        </>
+    );
 };
 
 export const ProfessionSelectionButtonMemoized = React.memo(ProfessionSelectionButton);
--- src/features/profession/ProfessionSelector.tsx
+++ src/features/profession/ProfessionSelector.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProfessionSelectionButtonMemoized } from "./ProfessionSelectionButton";
 
 export const ProfessionSelector = () => {
-    const availableProcessions = [
+    const [selections, setSelections] = useState<string[]>([]);
+
+    const availableProfessions = [
         "Alchemy",
         "Blacksmithing",
         "Enchanting",
@@ -19,8 +21,32 @@ export const ProfessionSelector = () => {
         "Archaeology"
     ];
 
-    const elements = availableProcessions.map((profession) => (
-        <ProfessionSelectionButtonMemoized key={profession} professionName={profession} />
+    const handleOnClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const value = event.currentTarget.value;
+        console.log(event);
+
+        if (selections.includes(value)) {
+            const newSelections = selections.filter((selection) => selection !== value);
+            setSelections(newSelections);
+
+            return;
+        }
+
+        const newSelections = [...selections, value];
+        if (newSelections.length > 2) {
+            newSelections.shift();
+        }
+
+        setSelections(newSelections);
+    };
+
+    const elements = availableProfessions.map((profession) => (
+        <ProfessionSelectionButtonMemoized
+            key={profession}
+            professionName={profession}
+            selections={selections}
+            handleOnClick={handleOnClick}
+        />
     ));
 
     return <div>{elements}</div>;
