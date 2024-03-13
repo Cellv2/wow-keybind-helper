75b34e3b3096e172507d04559985aff62ea07935
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-03-13 20:57:49 +0000

    Fix whole page styling

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ type Props = { children?: React.ReactNode };
 
 export const MainLayout = (props: Props) => {
     return (
-        <main className="dark:bg-blue-950 dark:text-white h-full">
+        <main className="dark:bg-blue-950 dark:text-white min-h-dvh">
             <Header />
             <Outlet />
         </main>
