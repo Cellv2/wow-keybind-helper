f3146d793e0c6ec09a00eaa30a6a27ed8e082f2c
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2024-11-25 21:14:54 +0000

    Add more testing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { queryByTestId, render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import App from "./App";
 
 describe("App", () => {
     it("renders the App component", () => {
         render(<App />);
 
+        // https://testing-library.com/docs/dom-testing-library/cheatsheet/#text-match-options
+        // https://vitest.dev/api/
+
+        const viteReactElement = screen.getByText("Vite + React");
+        expect(viteReactElement).toBeInTheDocument();
+
+        expect(
+            queryByTestId(document.documentElement, "does-not-exist")
+        ).not.toBeInTheDocument();
+
         // screen.debug(); // prints out the jsx in the App component unto the command line
+    });
+});
+
+describe("A truthy statement", () => {
+    it("should be equal to 2", () => {
+        expect(1 + 1).toEqual(2);
     });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
+// https://vitest.dev/config/file.html
+// https://vitest.dev/guide/coverage.html
 export default defineConfig({
     plugins: [react()],
     test: {
         environment: "jsdom",
-        setupFiles: './src/tests/setup-tests.ts',
+        setupFiles: "./src/tests/setup-tests.ts",
     },
 });
