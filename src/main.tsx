ef84ae418d1117288d5f289838a50d4567df2835
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-06-04 00:20:26 +0100

    Add svgr types

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
-if (!root) {
-    console.error("No root element found - panic?");
-} else {
+
     createRoot(root).render(
         <StrictMode>
             <App />
         </StrictMode>
     );
-}
