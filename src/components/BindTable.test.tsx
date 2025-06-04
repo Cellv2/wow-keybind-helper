8b93e7431564366ecdde37f64ec1380b16e8f40a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-06-04 23:55:58 +0100

    Add initial tests

---
+++ src/components/BindTable.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it } from "vitest";
+
+describe("BindTable", () => {
+    it("should have the number of rows passed in", () => {});
+    it("should have header cells", () => {});
+    it("should have the correct number of columns", () => {});
+});
