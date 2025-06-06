8d198eceee307427c6a885b5bf1c4000011c8031
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-06-06 22:41:35 +0100

    Refactor some tests

--- src/components/KeybindTable.test.tsx
+++ src/components/KeybindTable.test.tsx
@@ -2,20 +2,46 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { KeybindTable } from "./KeybindTable";
 
+// TODO: save query cheatsheet somewhere
+// https://testing-library.com/docs/react-testing-library/cheatsheet/#queries
+
+// TODO: see if we can get this working
+// https://eslint.style/rules/padded-blocks
+
 describe("BindTable", () => {
-    it("should have the number of rows passed in", () => {
+    it.todo("should have the number of rows", () => {
         render(<KeybindTable />);
     });
+
+    it.todo("should have the correct number of columns", () => {});
 
     it("should have header cells, one for ability and one for keybind", () => {
         render(<KeybindTable />);
 
-        const headers = screen.getAllByRole("columnheader");
-        const headerValues = headers.map((header) => header.innerText);
+        // const headers = screen.getAllByRole("columnheader");
+        // const headerValues = headers.map((header) => header.innerText);
 
-        expect(headers.length).toBe(2);
-        expect(headerValues.sort()).toEqual(["Abilities", "Keybind"].sort());
+        // TODO: read this + save it? https://dev.to/marktnoonan/why-i-rarely-use-getbyrole-testing-library-and-the-first-rule-of-aria-4581
+        const abilityHeader = screen.getByText("Ability", { selector: "th" });
+        const KeybindHeader = screen.getByText("Keybind", { selector: "th" });
+
+        expect(abilityHeader).toBeInTheDocument();
+        expect(KeybindHeader).toBeInTheDocument();
+
+        // expect(headers.length).toBe(2);
+        // expect(headerValues.sort()).toEqual(["Ability", "Keybind"].sort());
     });
 
-    it("should have the correct number of columns", () => {});
+    it.todo.each([
+        "Blink",
+        "Cone of Cold",
+        "Arcane Explosion",
+        "Arcane Intellect",
+    ])("should have %s in the table cells", (ability) => {
+        render(<KeybindTable />);
+
+        const match = screen.getByText(ability, { selector: "td" });
+
+        expect(match).toBeInTheDocument();
+    });
 });
