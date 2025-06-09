3d6b9c6fc42ca091217a96859b57803dd8b91e19
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-06-09 01:12:57 +0100

    Add some more tests and make them green

--- src/components/KeybindTable.test.tsx
+++ src/components/KeybindTable.test.tsx
@@ -9,14 +9,41 @@ import { KeybindTable } from "./KeybindTable";
 // https://eslint.style/rules/padded-blocks
 
 describe("BindTable", () => {
-    it.todo("should have the number of rows", () => {
-        render(<KeybindTable />);
-    });
+    it.each([
+        [{ abilities: ["One Ability"] }, 1],
+        [
+            {
+                abilities: ["Ability 1", "Ability Two", "Ability Three"],
+            },
+            3,
+        ],
+    ])(
+        "should have the correct number of ability rows for : %o",
+        (a, expected) => {
+            render(
+                <KeybindTable
+                    headers={["Header 1", "Header 2"]}
+                    abilities={a.abilities}
+                />
+            );
+
+            const matches = a.abilities.map((ability) =>
+                screen.getByText(ability, { selector: "td" })
+            );
+
+            expect(matches.length).toBe(expected);
+        }
+    );
 
     it.todo("should have the correct number of columns", () => {});
 
     it("should have header cells, one for ability and one for keybind", () => {
-        render(<KeybindTable />);
+        render(
+            <KeybindTable
+                headers={["AbilityTestHeader", "KeybindTestHeader"]}
+                abilities={[]}
+            />
+        );
 
         // const headers = screen.getAllByRole("columnheader");
         // const headerValues = headers.map((header) => header.innerText);
@@ -31,21 +58,49 @@ describe("BindTable", () => {
 
         expect(abilityHeader).toBeInTheDocument();
         expect(KeybindHeader).toBeInTheDocument();
-
-        // expect(headers.length).toBe(2);
-        // expect(headerValues.sort()).toEqual(["Ability", "Keybind"].sort());
     });
 
-    it.todo.each([
-        "Ability 1",
-        "RandomAbility",
-        "This One Has Spaces",
-        "More Intelligence Please",
-    ])("should have %s in the table cells", (ability) => {
-        render(<KeybindTable />);
+    it.each([
+        {
+            abilities: ["JustOneAbility"],
+        },
+        {
+            abilities: [
+                "Ability 1",
+                "RandomAbility",
+                "This One Has Spaces",
+                "More Intelligence Please",
+            ],
+        },
+        { abilities: ["Shaman Ability 1", "Shaman Ability 2"] },
+    ])("should have one td for each ability : %o", (a) => {
+        render(
+            <KeybindTable
+                headers={["abilityTestHeader", "KeybindTestHeader"]}
+                abilities={a.abilities}
+            />
+        );
 
-        const match = screen.getByText(ability, { selector: "td" });
+        a.abilities.forEach((ability) => {
+            const match = screen.getByText(ability, { selector: "td" });
 
-        expect(match).toBeInTheDocument();
+            expect(match).toBeInTheDocument();
+        });
+    });
+
+    it.each([
+        { abilities: ["AbilityTest"] },
+        {
+            abilities: ["AbilityTest", "Another Ability", "Test 3"],
+        },
+    ])("should have an associated keybind td for each ability : %o", (a) => {
+        render(<KeybindTable headers={[]} abilities={a.abilities} />);
+
+        a.abilities.forEach((ability) => {
+            const match = screen.getByText(ability, { selector: "td" });
+            const associatedKeybind = match.nextElementSibling;
+
+            expect(associatedKeybind).not.toBeNull();
+        });
     });
 });
--- src/components/KeybindTable.tsx
+++ src/components/KeybindTable.tsx
@@ -1,5 +1,27 @@
-type Props = {};
+type Props = {
+    headers: string[];
+    abilities: string[];
+};
 
 export const KeybindTable = (props: Props) => {
-    return <div>KeybindTable</div>;
+    const { abilities, headers } = props;
+    // return <div>KeybindTable</div>;
+    const headerElements = headers.map((header) => (
+        <th key={header}>{header}</th>
+    ));
+    const bodyElements = abilities.map((ability) => (
+        <tr>
+            <td key={`${ability}Name`}>{ability}</td>
+            <td key={`${ability}Bind`}></td>
+        </tr>
+    ));
+
+    return (
+        <table>
+            <thead>
+                <tr>{headerElements}</tr>
+            </thead>
+            <tbody>{bodyElements}</tbody>
+        </table>
+    );
 };
