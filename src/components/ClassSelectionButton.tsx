baa1c91fe284c5c83f060212c7ca840e7d974f4a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-07-02 23:13:57 +0100

    Add initial set of player class icons

--- src/components/ClassSelectionButton.tsx
+++ src/components/ClassSelectionButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const ClassSelectionButton = (props: Props) => {
     const { className, handleSetActiveClass, isActive = false } = props;
 
-    const componentClasses = ["size-16", "bg-sky-500/50"];
+    const componentClasses = ["size-16", "bg-sky-500/50", "w-12", "h-12"];
     if (isActive) {
         componentClasses.push("active");
     }
@@ -15,6 +15,7 @@ export const ClassSelectionButton = (props: Props) => {
     return (
         <button
             className={componentClasses.join(" ")}
+            // style={{height: "24px", width: "24px"}}
             onClick={handleSetActiveClass}
             title={className}
         >
