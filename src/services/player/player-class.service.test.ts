cf49541be7168388b01b1670cd7b665a489a6295
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-07-07 23:26:22 +0100

    Add base files and tests for player class svc

---
+++ src/services/player/player-class.service.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest";
+
+describe("player-class service", () => {
+    describe("getPlayerClassData", () => {
+        it.skip("should return the data for the correct class", () => {});
+    });
+
+    describe("getPlayerClassesAvailableForExpansion", () => {
+        it.skip("should return the classes available for the provided expansion", () => {});
+    });
+});
