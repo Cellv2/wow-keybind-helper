6fb6e4314b9dcb2f841eec4e8743bbefef092e08
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-07-22 23:16:32 +0100

    Add some tests and folder keeps

---
+++ src/services/player/player.service.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from "vitest";
+
+describe("player-class service", () => {
+    describe("getDataForPlayerClass", () => {
+        it.skip("should return the data for the provided player class", () => {});
+    });
+
+    describe("getPlayerClassesForExpansion", () => {
+        it.skip("should return the classes available for the provided expansion", () => {});
+    });
+
+    describe("getPlayerRacesForExpansion", () => {
+        it.skip("should return all player races for the provided expansion", () => {});
+        it.skip("should be possible to determine the races per faction", () => {});
+        it.skip("should show pandas... once? twice? I dunno yet", () => {});
+    });
+});
